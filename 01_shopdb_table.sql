0a00c268add093046c6badcee655f86eae5b46b4
Yerial <a01063135213@gmail.com>
2023-03-15 13:50:03 +0900

    3.15_1

---
+++ 01_shopdb_table.sql
@@ -0,0 +1,31 @@
+use shopdb;
+
+-- create table product (
+-- 	prdNo varchar(10) not null primary key,
+--     prdNameproduct varchar(30) not null,
+--     prdPrice int,
+--     prdCompany varchar(30)
+--     );
+
+-- create table product1 (
+--  	prdNo varchar(10) not null,
+--      prdNameproduct varchar(30) not null,
+--      prdPrice int,
+--      prdCompany varchar(30),
+--      constraint PK_product_prdNo Primary key (prdNo)
+--      );
+
+create -- table product2 (
+--  	prdNo varchar(10) not null,
+--      prdNameproduct varchar(30) not null,
+--      prdPrice int,
+--      prdCompany varchar(30),
+--      Primary key (prdNo)
+--      );
+
+-- create table publisher (
+-- 	pubno varchar(10) not null primary key,
+--     pubName varchar(30) not null
+--     );
+    
+create
